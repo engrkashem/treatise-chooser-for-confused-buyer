655b52eff0d69cc83f2be867d171136fbb9cc6ac
engrkashem <kashemaust@gmail.com>
2022-03-26 00:28:02 +0600

    nav bar added & readme file edited

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../../images/logo.png'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav>
+            <div className="header">
+                <img src={logo} alt="" />
+                <div>
+                    <a href="/home">Home</a>
+                    <a href="/products">Products</a>
+                    <a href="/favourites">Favourites</a>
+                    <a href="/about">About</a>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
