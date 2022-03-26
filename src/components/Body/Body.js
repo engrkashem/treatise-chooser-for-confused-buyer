2f55b4518425430cf6bfccfe64695fe5a1785741
engrkashem <kashemaust@gmail.com>
2022-03-26 13:44:50 +0600

    choose agia button working

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -8,7 +8,7 @@ const Body = () => {
 
     const [products, setProducts] = useState([]);
 
-    const [cart, setCart] = useState([])
+    const [carts, setCarts] = useState([])
 
     useEffect(() => {
         fetch('fakeDb.json')
@@ -17,14 +17,64 @@ const Body = () => {
     }, [])
 
     const addToCart = (book) => {
-        if (cart.length < 4) {
-            const newCart = [...cart, book];
-            setCart(newCart)
+        if (carts.length < 4) {
+            const newCart = [...carts, book];
+            setCarts(newCart)
+
         }
         else {
             alert('Oppps!!! You Have added maximum')
         }
     }
+
+    const generateRandomId = () => {
+        const randomID = Math.floor(Math.random() * 100)
+        if (randomID < 13) {
+            return randomID
+        }
+        else {
+            generateRandomId()
+        }
+    }
+
+    const matchRandomIdWithCartId = () => {
+
+        const randomID = generateRandomId()
+        let matchedCart = [];
+        for (const cart of carts) {
+            if (cart.id === randomID) {
+                matchedCart.push(cart)
+                return matchedCart
+            }
+        }
+        if (matchedCart === []) {
+            matchRandomIdWithCartId()
+        }
+        // return matchedCart;
+    }
+
+    const chooseFromCart = () => {
+        const matchedCart = matchRandomIdWithCartId()
+        console.log(matchedCart)
+        setCarts(matchedCart)
+
+    }
+
+    const removeCart = () => {
+        setCarts([])
+    }
+
+    /*  const chooseOneRandomly = (cart) => {
+         const randomID = Math.floor(Math.random() * 10)
+         // console.log(cart.id, randomID)
+         if (cart.id === randomID) {
+             const newCart = [cart]
+             setCarts(newCart)
+         }
+         else {
+             chooseOneRandomly(cart)
+         }
+     } */
 
     return (
         <div className='product-cart-container'>
@@ -40,17 +90,17 @@ const Body = () => {
             <div className="cart-container">
                 <div className='cart-info'>
                     <h3>Favourite Book Cart</h3>
-                    <h5>Item {cart.length} Added</h5>
+                    <h5>Item {carts.length} Added</h5>
                     {
-                        cart.map(book => <Cart
+                        carts.map(book => <Cart
                             key={book.id}
                             book={book}
                         ></Cart>)
                     }
-                    <button className='btn-choose btn-for-me'>
+                    <button onClick={chooseFromCart} className='btn-choose btn-for-me'>
                         <strong>CHOOSE 1 FOR ME</strong>
                     </button>
-                    <button className='btn-choose btn-again'>
+                    <button onClick={removeCart} className='btn-choose btn-again'>
                         <strong>CHOOSE AGAIN</strong>
                     </button>
                 </div>
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import './Cart.css'
 
 const Cart = ({ book: { id, name, img, author, price } }) => {
 
     return (
         <div>
-            <div>
-
+            <div className='cart-item'>
+                <img src={img} alt="" />
+                <h6 className='cart-item-name'>{name.slice(0, 15)}...</h6>
+                <div className='trash'>
+                    <FontAwesomeIcon icon={faTrashCan}></FontAwesomeIcon>
+                </div>
             </div>
 
         </div>
